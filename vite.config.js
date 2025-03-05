72ef7980eeb18aaace9f0ea654564d5f4ad5708d
jon-edward <townsend.jonathan.e@gmail.com>
2025-03-05 18:36:54 -0500

    Remove headers, update ads.txt

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import wasm from "vite-plugin-wasm";
 import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
 
 export default defineConfig({
-  server: { https: true },
+  server: {
+    https: true,
+  },
   plugins: [wasm(), mkcert(), crossOriginIsolation()],
   build: {
     target: "ES2022",
